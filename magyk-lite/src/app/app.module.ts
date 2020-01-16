8e8766e963822d6cac651a3121d02ebf50b81961
David <charlesinvestingltd@gmail.com>
2020-01-15 21:25:57 -0500

    added tool bar with colors

--- magyk-lite/src/app/app.module.ts
+++ magyk-lite/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +16,7 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
