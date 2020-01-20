2556dc484717757e2c3d39edebaabc0ac67283b1
David <charlesinvestingltd@gmail.com>
2020-01-20 13:59:29 -0500

    added timesheet

--- magyk-lite/src/app/app.module.ts
+++ magyk-lite/src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { TimesheetComponent } from './timesheet/timesheet.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomePageComponent
+    HomePageComponent,
+    TimesheetComponent
   ],
   imports: [
     BrowserModule,
